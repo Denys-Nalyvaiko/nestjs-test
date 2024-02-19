6fcb813458c6134112155ddf7654eb883f13f0b6
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-02-19 21:29:50 +0100

    cahnge cats response

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,22 +8,31 @@ import {
   Redirect,
   Param,
   Body,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import CreateCatDTO from './create-cat.dto';
 import UpdateCatDTO from './update-cat.dto';
 
 @Controller('/cats')
 class CatsController {
   @Post()
-  async create(@Body() createCatDTO: CreateCatDTO): Promise<string> {
-    console.log(createCatDTO);
-    return 'This action create new cat';
+  async create(
+    @Body() createCatDTO: CreateCatDTO,
+    @Res() res: Response,
+  ): Promise<any> {
+    res.status(HttpStatus.CREATED).json(createCatDTO);
   }
 
   @Get()
-  async findAll(@Req() request: Request): Promise<string> {
-    return `This action returns all cats whit url ${request.url}`;
+  async findAll(
+    @Req() request: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<any> {
+    console.log(`This action returns all cats whit url ${request.url}`);
+    res.status(HttpStatus.OK);
+    return [];
   }
 
   @Get(':id')
