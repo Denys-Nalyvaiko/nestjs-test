66364fdb94201458d8e9484ecf42380e83319e10
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-02-26 19:27:59 +0100

    configure middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { AuthMiddleware } from './common/middleware/auth.middleware';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [CatsModule, UsersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, AuthMiddleware)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'users', method: RequestMethod.PATCH },
+      )
+      .forRoutes({ path: 'users', method: RequestMethod.ALL });
+  }
+}
