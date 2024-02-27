013b25e0c31409700c610f9bc3c29e9e40b82277
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2024-02-27 23:17:08 +0100

    configure guards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Post, Body, UsePipes } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UsePipes,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateCatDTO, createCatsSchema } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cats.interface';
 import { ZodValidationPipe } from 'src/common/pipe/zod-validation.pipe';
+import { RoleGuard } from 'src/roles/roles.guard';
+import { Roles } from 'src/roles/roles.decorator';
 
 @Controller('/cats')
+@UseGuards(RoleGuard)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
@@ -14,6 +24,7 @@ export class CatsController {
   }
 
   @Post()
+  @Roles(['admin'])
   @UsePipes(new ZodValidationPipe(createCatsSchema))
   async create(@Body() createCatDTO: CreateCatDTO) {
     this.catsService.create(createCatDTO);
